1205d51b950904043f6af6a5a6868c9247781a58
sourav santra <souravsantra59580@gmail.com>
2020-08-01 08:40:26 +0530

    9th commit(01/08/2020)

--- Linklist/GroupReverse.java
+++ Linklist/GroupReverse.java
@@ -13,16 +13,65 @@ public class GroupReverse {
             size++;
             temp = temp.next;
         }
+        //System.out.println(size);
         return size;
     }
+
+    //fast move
+    static Node fastMove(Node head,int k){
+        Node temp = head;
+        while(k>0){
+            temp = head.next;
+            k--;
+        }
+        return temp.next;
+    }
+
+    //slow move
+    static Node slowMove(Node head,int k){
+        Node temp = head;
+        while(k>0){
+            temp = head.next;
+            k--;
+        }
+        return temp.next;
+    }
+   
+    static void swap(Node fast, Node slow,int k)
+    {
+        while(k>0)
+        {
+            int t = slow.data;
+            slow.data = fast.data;
+            fast.data = t;
+            fast= fast.next;
+            slow= slow.next;
+            k--;
+        }
+    }
+
+
+
+
+
     static Node reverse(Node head,int k)
     {
        int size = GetSize(head);
+       int n = k+k;
+       int rns= n; 
+       Node temp = head;
+       Node fast = head;
+       fast = fastMove(fast,k);
+       Node slow = head;
 
-       while(size>k){
-           
+       while(fast!=null){
+         n=n+k+k;
+         swap(fast,slow,k);
+         fast = fastMove(fast,k);
+         slow = slowMove(fast,k);
+         if(size<n)
+         break;
        }
-
 
        return head;
     }
@@ -34,7 +83,8 @@ public class GroupReverse {
         head.next.next.next = new Node(6);
         head.next.next.next.next = new Node(7);
         head.next.next.next.next.next = new Node(8);
-
+        head.next.next.next.next.next.next = new Node(9);
+        // head.next.next.next.next.next.next.next = new Node(10);
 
         head = reverse(head,2);
         Node temp = head; 
